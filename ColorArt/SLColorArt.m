34b0d30d08905ad9d688f3e8805b094c6fbaf754
Eric Gallager <egallager@comcast.net>
2012-12-30 20:14:08 -0500

    Fix for Snow Leopard

--- ColorArt/SLColorArt.m
+++ ColorArt/SLColorArt.m
@@ -1,19 +1,21 @@
-//
-//  SLColorArt.m
-//  ColorArt
-//
-//  Created by Aaron Brethorst on 12/11/12.
-//
-// Copyright (C) 2012 Panic Inc. Code by Wade Cosgrove. All rights reserved.
-//
-// Redistribution and use, with or without modification, are permitted provided that the following conditions are met:
-//
-// - Redistributions must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
-//
-// - Neither the name of Panic Inc nor the names of its contributors may be used to endorse or promote works derived from this software without specific prior written permission from Panic Inc.
-//
-// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL PANIC INC BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
+/*
+ *  SLColorArt.m
+ *  ColorArt
+ *
+ *  Created by Aaron Brethorst on 12/11/12.
+ *
+ * Copyright (C) 2012 Panic Inc. Code by Wade Cosgrove. All rights reserved.
+ *
+ * Redistribution and use, with or without modification, are permitted provided that the following conditions are met:
+ *
+ * - Redistributions must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+ *
+ * - Neither the name of Panic Inc nor the names of its contributors may be used to endorse or promote works derived from this software without specific prior written permission from Panic Inc.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL PANIC INC BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
 
 #import "SLColorArt.h"
 
@@ -59,10 +61,10 @@
     if (self)
     {
         self.scaledSize = size;
-		
+
 		NSImage *finalImage = [self scaleImage:image size:size];
 		self.scaledImage = finalImage;
-		
+
 		[self analyzeImage:image];
     }
 
@@ -190,7 +192,7 @@
 		NSUInteger colorCount = [leftEdgeColors countForObject:curColor];
 
         NSInteger randomColorsThreshold = (NSInteger)(pixelsHigh * kColorThresholdMinimumPercentage);
-        
+
 		if ( colorCount <= randomColorsThreshold ) // prevent using random colors, threshold based on input image height
 			continue;
 
@@ -281,7 +283,7 @@
 		{
 			if ( ![*secondaryColor pc_isDistinct:curColor] || ![*primaryColor pc_isDistinct:curColor] || ![curColor pc_isContrastingColor:backgroundColor] )
 				continue;
-            
+
 			*detailColor = curColor;
 			break;
 		}
@@ -417,7 +419,6 @@
 	return YES;
 }
 
-
 @end
 
 
@@ -449,9 +450,9 @@
 			return NSOrderedSame;
 		}
 	}
-    
+
 	return NSOrderedAscending;
 }
 
-
 @end
+
---
+++ Readme.md
@@ -0,0 +1,1 @@
+Panic Inc's ColorArt code, forked to get it to work better on Snow Leopard
